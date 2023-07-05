d6622f4eecf96668ff03d9db8d3d51c2507dbde0
crackun24 <53938436+crackun24@users.noreply.github.com>
2023-07-05 22:34:01 +0800

    初始化提交

---
+++ src/main/java/com/mininglist/thestarrymininglist/TheStarryMiningList.java
@@ -0,0 +1,10 @@
+package com.mininglist.thestarrymininglist;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TheStarryMiningList implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/mininglist/thestarrymininglist/client/TheStarryMiningListClient.java
@@ -0,0 +1,11 @@
+package com.mininglist.thestarrymininglist.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class TheStarryMiningListClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
