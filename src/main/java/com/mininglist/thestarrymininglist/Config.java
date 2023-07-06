baaa6ca627dc9e41b7008899e08ffea4eefb678f
crackun24 <53938436+crackun24@users.noreply.github.com>
2023-07-06 12:13:32 +0800

    完成

--- src/main/java/com/mininglist/thestarrymininglist/Config.java
+++ src/main/java/com/mininglist/thestarrymininglist/Config.java
@@ -3,8 +3,7 @@ package com.mininglist.thestarrymininglist;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 //配置类
@@ -13,16 +12,42 @@ public class Config {
     private Properties mProp;
     private final Logger LOGGER = LogManager.getLogger();//日志记录器
 
-    public Config(final String url) {//构造函数,传入配置文件的地址
+    private final String DEFAULT_CONFIG_DATA = "ScoreboardDisplayName = starryBoard\n" +
+            "ScoreboardName = starryBoard";
+
+    private void CreateDefaultConfigFile(File file) throws IOException {
+        file.createNewFile();//创建一个新的配置文件
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file)))//创建新的文件写入对象
+        {
+            writer.write(DEFAULT_CONFIG_DATA);//写入默认的配置文件信息
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }//创建默认的配置文件
+
+    public Config(final String filePath) {//构造函数,传入配置文件的地址
+        File file = new File(filePath + "\\" + CONFIG_FILE_NAME);//拼接成正确的配置文件路径
+
+        if (!file.exists())//判断配置文件是否存在
+        {
+            try {
+                CreateDefaultConfigFile(file);//如果配置文件不存在,则直接创建默认的配置文件
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         this.mProp = new Properties();//创建新的properties文件读取对象
 
-        LOGGER.info(new String("Reading config %s.").formatted(CONFIG_FILE_NAME));
-
-        try (InputStream input = new FileInputStream(url)) {//创建输入流
-
+        try (InputStream input = new FileInputStream(file)) {//创建输入流
+            this.mProp.load(input);//加载配置文件
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public String GetValue(final String key)//获取配置文件对应的值
+    {
+        return this.mProp.getProperty(key);//返回值
+    }
 }
--- src/main/java/com/mininglist/thestarrymininglist/TheStarryMiningList.java
+++ src/main/java/com/mininglist/thestarrymininglist/TheStarryMiningList.java
@@ -2,22 +2,62 @@ package com.mininglist.thestarrymininglist;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardCriterion;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.scoreboard.ScoreboardPlayerScore;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class TheStarryMiningList implements ModInitializer {
     private static final Logger LOGGER = LogManager.getLogger();
+    private Scoreboard mScoreboard;//计分板对象
+    private ScoreboardObjective mScoreboardObj;//计分板的计分对象
+
+    private void CreateScoreboard(final String name,final String display_name) {
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            this.mScoreboard = server.getWorld(World.OVERWORLD).getScoreboard();//获取世界的计分板
+
+            this.mScoreboardObj = mScoreboard.getObjective(name);//获取服务器的计分板对象
+
+            if (mScoreboardObj == null) {//判断是否为空对象
+                this.mScoreboardObj = mScoreboard.addObjective(name, ScoreboardCriterion.DUMMY, Text.literal(display_name),
+                        ScoreboardCriterion.RenderType.INTEGER);
+                this.mScoreboard.setObjectiveSlot(1,this.mScoreboardObj);//设置显示的位置
+            }
+        });
+    }
+
+    private void HookPlayerBreakBlockEvent() {//设置玩家破坏方块事件的回调
+        PlayerBlockBreakEvents.AFTER.register(((world, player, pos, state, blockEntity) -> {
+            ScoreboardPlayerScore score = this.mScoreboard.getPlayerScore(player.getName().getString(),
+                    this.mScoreboardObj);//获取玩家计分对象
+
+            int player_score = score.getScore();//获取玩家当前的计分数
+            player_score++;//分数递增
+            score.setScore(player_score);//更新玩家的分数
+        }));
+    }
+
     @Override
     public void onInitialize() {
         FabricLoader loader = FabricLoader.getInstance();//获取加载器的实例
+        File config_file_path = loader.getConfigDir().toFile();//获取配置文件
 
-        String configPath = loader.getConfigDirectory().getPath();//获取配置文件的路径
-        Config config = new Config(configPath);//读取配置文件
+        Config config = new Config(config_file_path.getPath());//读取配置文件
+
+        String name = config.GetValue("ScoreboardDisplayName");//获取计分板的名字
+        String disPlayName = config.GetValue("ScoreboardName");//获取计分板显示的名字
+
+        CreateScoreboard(name,disPlayName);//创建计分板
+        HookPlayerBreakBlockEvent();//设置玩家破坏方块事件的回调
     }
 }
