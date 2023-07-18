53da81f0a6d077919e1ad005c9c52123c4b05ac4
crackun24 <53938436+crackun24@users.noreply.github.com>
2023-07-18 13:18:21 +0800

    跨版本的冲突优化

--- src/main/java/com/mininglist/thestarrymininglist/TheStarryMiningList.java
+++ src/main/java/com/mininglist/thestarrymininglist/TheStarryMiningList.java
@@ -1,5 +1,8 @@
 package com.mininglist.thestarrymininglist;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.api.ModInitializer;
 
 //#if MC < 11900
@@ -16,7 +19,11 @@ import net.minecraft.scoreboard.ScoreboardObjective;
 import net.minecraft.scoreboard.ScoreboardPlayerScore;
 //#if MC >= 11900
 //$$ import net.minecraft.text.Text;
+//$$ import net.minecraft.server.command.ServerCommandSource;
+//$$ import net.minecraft.server.command.CommandManager;
 //#else
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
 //#endif
 import net.minecraft.text.Text;
@@ -38,16 +45,35 @@ public class TheStarryMiningList implements ModInitializer {
                 //$$ CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("TheStarryMiningList")
                 //#endif
                 .executes(context -> {
-                    if(isScoreboardVisible) {
+                    if (isScoreboardVisible) {
                         this.mScoreboard.setObjectiveSlot(1, this.mScoreboardObj); // 显示计分板
-                        context.getSource().getPlayer().sendMessage(Text.of("计分板已开启"),true);
+                        context.getSource().getPlayer().sendMessage(Text.of("已启用计分板"),true);
                     } else {
                         this.mScoreboard.setObjectiveSlot(1, null); // 隐藏计分板
-                        context.getSource().getPlayer().sendMessage(Text.of("计分板已关闭"),true);
+                        context.getSource().getPlayer().sendMessage(Text.of("已关闭计分板"),true);
                     }
                     isScoreboardVisible = !isScoreboardVisible;
                     return 1;
                 })));
+            //#if MC >= 11900
+            //$$ CommandRegistrationCallback.EVENT.register((dispatcher, dedicated,environment) -> {
+            //#else
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            //#endif
+            LiteralCommandNode<ServerCommandSource> command = dispatcher.register(
+                    CommandManager.literal("setStarryBoardDisplayName")
+                            .then(CommandManager.argument("scoreBoardName", StringArgumentType.word())
+                                    .then(CommandManager.argument("scoreBoardDisplayName", StringArgumentType.word())
+                                            .executes(context -> {
+                                                String scoreboardName = StringArgumentType.getString(context, "scoreboardName");
+                                                String displayName = StringArgumentType.getString(context, "displayName");
+                                                setScoreBoardDisplayName(scoreboardName, displayName, context.getSource());
+                                                return 1;
+                                            })
+                                    )
+                            )
+            );
+        });
 
         FabricLoader loader = FabricLoader.getInstance();//获取加载器的实例
         File config_file_path = loader.getConfigDir().toFile();//获取配置文件
@@ -62,18 +88,39 @@ public class TheStarryMiningList implements ModInitializer {
     private ScoreboardObjective mScoreboardObj; //计分板的计分对象
     private boolean isScoreboardVisible = false; //计分板的开关状态
 
+    //更改计分板显示名称
+    private void setScoreBoardDisplayName(String scoreboardName, String displayName, ServerCommandSource source) {
+        Scoreboard scoreboard = source.getMinecraftServer().getScoreboard();
+        ScoreboardObjective objective = scoreboard.getNullableObjective(scoreboardName);
+        if (objective != null) {
+            objective.setDisplayName(Text.of(displayName));
+
+            try {
+                source.getPlayer().sendMessage(Text.of("Scoreboard display name set to: " + scoreboardName),true);
+            } catch (CommandSyntaxException e) {
+               e.printStackTrace();
+            }
+        } else {
+            try {
+                source.getPlayer().sendMessage(Text.of("Scoreboard not found: " + scoreboardName),true);
+            } catch (CommandSyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     private void CreateScoreboard(final String name, final String display_name) {
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
             this.mScoreboard = Objects.requireNonNull(server.getWorld(World.OVERWORLD)).getScoreboard();//获取世界的计分板
             this.mScoreboardObj = mScoreboard.getObjective(name);//获取服务器的计分板对象
             if (mScoreboardObj == null) {//判断是否为空对象
-                    //#if MC < 11900
-                    this.mScoreboardObj = mScoreboard.addObjective(name, ScoreboardCriterion.DUMMY, new LiteralText(display_name),
-                            //#else
-                            //$$ this.mScoreboardObj = mScoreboard.addObjective(name, ScoreboardCriterion.DUMMY, Text.literal(display_name),
-                            //#endif
-                            ScoreboardCriterion.RenderType.INTEGER);
-                    this.mScoreboard.setObjectiveSlot(1, null);
+                //#if MC < 11900
+                this.mScoreboardObj = mScoreboard.addObjective(name, ScoreboardCriterion.DUMMY, new LiteralText(display_name),
+                        //#else
+                        //$$ this.mScoreboardObj = mScoreboard.addObjective(name, ScoreboardCriterion.DUMMY, Text.literal(display_name),
+                        //#endif
+                        ScoreboardCriterion.RenderType.INTEGER);
+                this.mScoreboard.setObjectiveSlot(1, null);
             }
         });
     }
